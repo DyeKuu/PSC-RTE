693b1fd8d963e0247327ff5afba609b62994fa6c
Kragon-Nox <55194205+Kragon-Nox@users.noreply.github.com>
2019-12-12 17:49:13 +0100

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,12 +1,15 @@
 import cplex
 import numpy as np
 
-#class representing linear optimization problems
+#class representing linear optimization problems. Will essentially be used to generate new linear optimization problems 
+#by adding some noise (gaussian) to some chosen coefficients of a given problem. 
 
 #Parameters: name, dev (stands for standard deviation, see comment of generate_random_prob),
-#prob (instance of the class cplex.Cplex, linear optimization problem)
+#            prob (instance of the class cplex.Cplex, linear optimization problem), non_fixed_vars (subset of the variables of the
+#            linear optimization problem, contains all variables we choose not to be fixed for all RTE-like problems and thus will be
+#            affected by the noise we apply while the generation of random new problems)
 
-class problem:
+class RTElike_lin_opt_problem:
     def __init__(self, prob_name):
         if isinstance(prob_name, str):
             self.dev = 0
@@ -14,16 +17,24 @@ class problem:
             prob = cplex.Cplex()
             prob.read(prob_name)
             self.prob = prob
+            self.non_fixed_vars = []
         if isinstance(prob_name, cplex.Cplex):
             self.dev = 0
             self.name = 'default'
             self.prob = prob_name
+            self.non_fixed_vars = []
 
     def set_deviation(self, dev):
         self.dev = dev
 
     def get_deviation(self):
         return self.dev
+    
+    def set_non_fixed_vars(self, non_fixed_vars):
+        self.non_fixed_vars = non_fixed_vars
+        
+    def get_non_fixed_vars(self):
+        return self.non_fixed_vars
 
 #method solves the linear optimization problem
 
@@ -46,16 +57,16 @@ class problem:
     def generate_random_prob(self, name):
         list_rhs = self.prob.linear_constraints.get_rhs()
         l = len(list_rhs)
-        for i in range(l):
-            val = list_rhs[i]
-            list_rhs[i] = val + np.random.normal(0, val*self.dev, 1)  # add gaussian noise to the RHS
+        for elem in non_fixed_vars:
+            val = list_rhs[elem]
+            list_rhs[elem] = val + np.random.normal(0, val*self.dev, 1)  # add gaussian noise to the RHS
         new_list = []
         for i in range(l):
             new_list.append((i, list_rhs[i]))
         new_prob = cplex.Cplex()  # create new problem
         new_prob.read(name)
         new_prob.linear_constraints.set_rhs(new_list)  # set the RHS to the bias RHS
-        new_problem = problem(new_prob)  # generate a new class
+        new_problem = RTElike_lin_opt_problem(new_prob)  # generate a new instance of the class
         return new_problem
 
 
@@ -75,11 +86,12 @@ class problem:
 
 #Test with dev = 0.1
 
-test = problem('petit_probleme.lp')
+test = RTElike_lin_opt_problem('petit_probleme.lp')
 test.set_deviation(0.1)  # set the delta of the gaussian noise
 test.solve()
+test.set_non_fixed_vars([1,2,4])
 print(test.prob.solution.get_objective_value())
 
-new_prob = test.generate_random_prob()  # generate a new problem from test
+new_prob = test.generate_random_prob('new_petit_probleme.lp')  # generate a new problem from test
 new_prob.solve()
 print(new_prob.prob.solution.get_objective_value())
