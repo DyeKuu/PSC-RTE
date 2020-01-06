13de436f14571a251fee27cfbefeef6c36c030c4
EtiMag <55194599+EtiMag@users.noreply.github.com>
2020-01-06 14:10:34 +0100

    Update dataset.py to correctly initialise the dataset

--- generator.py
+++ generator.py
@@ -191,9 +191,7 @@ def problem_generator(problems, N, dev, non_fixed_vars = None):
     prob_root.generate_random_prob_mult(N)
     RHS_list = prob_root.extract_RHS()
     sol_list = prob_root.calculate_solutions()
-    data = dataset.dataset() # rewrite to initialise the dataset instance
-    data.RHS = RHS_list # rewrite
-    data.solutions = sol_list # rewrite
+    data = dataset.dataset(RHS_list, sol_list)
     return data
     
 
