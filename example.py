803698823868498dcca931f51fff41359f58dd22
EtiMag <55194599+EtiMag@users.noreply.github.com>
2020-01-07 14:58:48 +0100

    Update on

---
+++ example.py
@@ -0,0 +1,8 @@
+from PSC-RTE-master.NeuralNetwork import nn
+from PSC-RTE-master.dataset import dataset, RHS, solutions
+from PSC-RTE-master.generator import lin_opt_pbs, problem_generator
+from PSC-RTE-master.analyze import to_analyze
+
+
+
+
